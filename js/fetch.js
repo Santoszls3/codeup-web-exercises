f039cd91f8553a524d625295e5882b90c3b39994
Santos Gonzales <Santoszls3@gmail.com>
2023-11-30 11:07:50 -0600

    new doc

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,29 @@
 
 
+// fetch("https://api.github.com/users/Santoszls3/events/public", {headers: {'Authorization': 'token ' + GH_KEY}})
+// function getGithubUsernames() {
+//     return fetch("https://api.github.com/users/Santoszls3/events/public", {headers: {'Authorization': 'token ' + GH_KEY}})
+//         .then(response => response.json())
+// }
+// // later on...
+//
+// getGithubUsernames().then( users => {
+//     users.forEach( userObj => {
+//         // do something with each username
+//         console.log(userObj);
+//     });
+// }).catch(error => console.error(error))
+
+
 fetch("https://api.github.com/users/Santoszls3/events/public", {headers: {'Authorization': 'token ' + GH_KEY}})
-function getGithubUsernames() {
+function getLastCommitDate(username) {
     return fetch("https://api.github.com/users/Santoszls3/events/public", {headers: {'Authorization': 'token ' + GH_KEY}})
-        .then(response => response.json())
+        .then(response => response.json());
+
 }
 // later on...
 
-getGithubUsernames().then( users => {
+getLastCommitDate().then( users => {
     users.forEach( userObj => {
         // do something with each username
         console.log(userObj);
