c5b6aee8cd7fec23f9602518f7d2f94b96b1aaf5
Santos Gonzales <Santoszls3@gmail.com>
2024-04-05 16:45:07 -0500

    new css

--- UDEM/js/Event-basics.js
+++ UDEM/js/Event-basics.js
@@ -8,9 +8,26 @@ button.addEventListener('click', function () {
     h1.innerText = newColor
 })
 
+
+const buttons2 = document.querySelectorAll('#button2 button'); // Select all buttons within the #button2 div
+
+for(let button of buttons2){
+    button.addEventListener('click', function () {
+        // below will change the color of the button
+        button.style.backgroundColor = makeRandomColor();
+        // below will change the color of the font in the button
+        button.style.color = makeRandomColor();
+    });
+}
+
 const makeRandomColor = () => {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`
 }
+
+
+document.querySelector('button').addEventListener('click', function (evt){
+    alert("click")
+})
